45878b93fcc7f7e84335206dc9968be9c5a6a5a5
ShashankDahake7 <shashankdahake578@gmail.com>
2024-01-14 00:43:36 +0530

    Added Solutions

---
+++ week-5/Assignments/level-1/Solution/component/BusinessCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './BusinessCard.css';
+
+export function BusinessCard(props) {
+    return (
+        <div className="card">
+            <h1 className="name">{props.name}</h1>
+            <p className="description">{props.description}</p>
+            <h2 className="interestsHeader">Interests</h2>
+            <ul className="interestsList">
+                {props.interests.map((interest) => (
+                    <li key={interest} className="interestItem">
+                        {interest}
+                    </li>
+                ))}
+            </ul>
+            <div className="socialLinks">
+                <a href={props.linkedin} target="_blank" rel="noopener noreferrer" className="link">
+                    LinkedIn
+                </a>
+                <br />
+                <a href={props.twitter} target="_blank" rel="noopener noreferrer" className="link">
+                    Twitter
+                </a>
+                {props.otherSocialMedia && (
+                    <a href={props.otherSocialMedia} target="_blank" rel="noopener noreferrer" className="link">
+                        {props.otherSocialMedia.label}
+                    </a>
+                )}
+            </div>
+        </div>
+    );
+}
--- week-5/Assignments/level-1/Solution/src/App.jsx
+++ week-5/Assignments/level-1/Solution/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import React from 'react';
+import BusinessCard from '../component/BusinessCard';
 
 function App() {
+  const businessCardData = {
+    name: 'Shashank',
+    description: 'Frontend Developer',
+    interests: ['React', 'JavaScript', 'Web Development'],
+    linkedin: 'https://www.linkedin.com/in',
+    twitter: 'https://twitter.com',
+    otherSocialMedia: {
+      url: 'https://www.instagram.com',
+      label: 'Instagram',
+    },
+  };
+
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <BusinessCard {...businessCardData} />
+    </div>
+  );
 }
 
-export default App
+export default App;

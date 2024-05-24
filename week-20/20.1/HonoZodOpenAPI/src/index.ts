63d06b03411ddac79ce8586c8849141ee9f49bcd
ShashankDahake7 <shashankdahake578@gmail.com>
2024-05-24 23:10:11 +0530

    Added Solutions

---
+++ week-20/20.1/HonoZodOpenAPI/src/index.ts
@@ -0,0 +1,74 @@
+import { createRoute } from '@hono/zod-openapi'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { ParamsSchema } from './outputs'
+import { UserSchema } from './inputs'
+import { swaggerUI } from '@hono/swagger-ui'
+
+const app = new OpenAPIHono()
+
+const getUserroute = createRoute({
+  method: 'get',
+  path: '/users/{id}',
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema,
+        },
+      },
+      description: 'Retrieve the user',
+    },
+  },
+})
+
+const postUserroute = createRoute({
+  method: 'post',
+  path: '/users/{id}',
+  request: {
+    params: ParamsSchema,
+  },
+  responses: {
+    200: {
+      content: {
+        'application/json': {
+          schema: UserSchema,
+        },
+      },
+      description: 'Retrieve the user',
+    },
+  },
+})
+
+app.openapi(getUserroute, (c) => {
+  const { id } = c.req.valid('param')
+  return c.json({
+    id,
+    age: 20,
+    name: 'Ultra-man',
+  })
+})
+
+app.openapi(postUserroute, (c) => {
+  const { id } = c.req.valid('param')
+  return c.json({
+    id,
+    age: 20,
+    name: 'Ultra-man',
+  })
+})
+
+// The OpenAPI documentation will be available at /doc
+app.doc('/doc', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+  },
+})
+
+app.get('/ui', swaggerUI({ url: '/doc' }))
+
+export default app

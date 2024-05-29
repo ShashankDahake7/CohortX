dfaee357c41b87d7302b7fc63315d61e1d62fe9f
ShashankDahake7 <shashankdahake578@gmail.com>
2024-05-29 23:58:43 +0530

    Added Solutions

---
+++ week-24/24.2/7-DeepMocking/src/index.ts
@@ -0,0 +1,124 @@
+// import express from "express";
+// import { z } from "zod";
+// import { prismaClient } from "./__mocks__/db";
+
+// export const app = express();
+// app.use(express.json());
+
+// const sumInput = z.object({
+//     a: z.number(),
+//     b: z.number()
+// })
+
+// app.post("/sum", async (req, res) => {
+//     const parsedResponse = sumInput.safeParse(req.body)
+
+//     if (!parsedResponse.success) {
+//         return res.status(411).json({
+//             message: "Incorrect inputs"
+//         })
+//     }
+
+//     const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+//     await prismaClient.sum.create({
+//         data: {
+//             a: parsedResponse.data.a,
+//             b: parsedResponse.data.b,
+//             result: answer
+//         }
+//     })
+
+//     res.json({
+//         answer
+//     })
+// });
+
+// app.get("/sum", (req, res) => {
+//     const parsedResponse = sumInput.safeParse({
+//         a: Number(req.headers["a"]),
+//         b: Number(req.headers["b"])
+//     })
+
+//     if (!parsedResponse.success) {
+//         return res.status(411).json({
+//             message: "Incorrect inputs"
+//         })
+//     }
+
+//     const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+//     res.json({
+//         answer
+//     })
+// });
+
+
+
+// Problem
+// What if we are using the return value from the database call?
+
+import express from "express";
+import { z } from "zod";
+import { prismaClient } from "./__mocks__/db";
+
+export const app = express();
+app.use(express.json());
+
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
+
+app.post("/sum", async (req, res) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+    
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    const response = await prismaClient.sum.create({
+        data: {
+            a: parsedResponse.data.a,
+            b: parsedResponse.data.b,
+            result: answer
+        }
+    })
+
+    res.json({
+        answer,
+        id: response.id
+    })
+});
+
+app.get("/sum", async (req, res) => {
+    const parsedResponse = sumInput.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    })
+    
+    if (!parsedResponse.success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    const response = await prismaClient.sum.create({
+        data: {
+            a: parsedResponse.data.a,
+            b: parsedResponse.data.b,
+            result: answer
+        }
+    })
+
+    res.json({
+        answer,
+        id: response.id
+    })
+});
